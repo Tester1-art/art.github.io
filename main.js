0f26ce5e8237b5bec337720e3289bb4b1d05b044
Tester1-art <62819926+Tester1-art@users.noreply.github.com>
2023-02-05 16:55:03 +0200

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// Example of a JavaScript animation
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-  header.style.opacity = 1 - +window.pageYOffset / 200 + "";
-});
